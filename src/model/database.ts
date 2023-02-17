a076a8c3e060970b2022f249f25e2841d048b417
jacobKdev <kjmetibemu@gmail.com>
2023-02-17 20:40:41 +0100

    minor fix

--- src/model/database.ts
+++ src/model/database.ts
@@ -1,6 +1,7 @@
 import mongoose  from "mongoose";
 
-const  DB_URL: any = process.env.DB_URL;
+// const  DB_URL: any = process.env.DB_URL;
+const DB_URL='mongodb+srv://jakobnewthing:pa$$word@cluster0.v6xyx9x.mongodb.net/?retryWrites=true&w=majority'
 export  const connect = async () => {
   try {
     await mongoose.connect(DB_URL);
